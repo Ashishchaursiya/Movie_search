9f79b872e850cc029734ef3c6915d0cd51b6cd21
Ashishchaursiya <chaurasiyaashish383@gmail.com>
2020-08-27 17:23:40 +0530

    Updates

---
+++ precache-manifest.62d959d87b28fc728171e61695f7469f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33437471c5b179f1cba98fcf54c6f657",
+    "url": "/Movie_search/index.html"
+  },
+  {
+    "revision": "7291aebc3a6991451b4c",
+    "url": "/Movie_search/static/css/main.27615b85.chunk.css"
+  },
+  {
+    "revision": "abf22650d419f8d5833e",
+    "url": "/Movie_search/static/js/2.1ec72ded.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie_search/static/js/2.1ec72ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7291aebc3a6991451b4c",
+    "url": "/Movie_search/static/js/main.302fadfd.chunk.js"
+  },
+  {
+    "revision": "55916d2b4ca17e12142b",
+    "url": "/Movie_search/static/js/runtime-main.f43260ff.js"
+  }
+]);
